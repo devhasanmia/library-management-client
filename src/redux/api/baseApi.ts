6000cc65f9a948c5b694a1453f185ef6f47101d7
devhasanmia <dev.hasanmia@gmail.com>
2025-07-03 22:59:32 +0600

    Base Url Change

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://library-management-api-lilac.vercel.app/api' }),
     tagTypes: ["Book", "borrow"],
     endpoints: () => ({})
 })
